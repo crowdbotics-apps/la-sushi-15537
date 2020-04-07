47b804e781f2b30d09f68d720de6c1842459e274
Crowdbotics <team@crowdbotics.com>
2020-04-07 03:49:48 +0000

    remove package blueprint-react-native-tutorial-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,7 +5,6 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
-import TutorialNavigator from '../features/Tutorial/navigator';
 import MapsNavigator from '../features/Maps/navigator';
 
 /**
@@ -17,7 +16,6 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
-Tutorial: { screen: TutorialNavigator },
 Maps: { screen: MapsNavigator },
 
     /** new navigators can be added here */
